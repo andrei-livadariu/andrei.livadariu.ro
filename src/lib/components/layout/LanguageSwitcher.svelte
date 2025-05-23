48ab1bf855d17485f0c83c43050eab34e266a3e6
Andrei Livadariu <livadariu.andrei@gmail.com>
2025-05-23 18:34:35 +0900

    Fix - The Language Switcher links now work without a trailing slash

--- src/lib/components/layout/LanguageSwitcher.svelte
+++ src/lib/components/layout/LanguageSwitcher.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import {m} from '$lib/paraglide/messages.js';
-    import {locales, localizeHref, getLocale, setLocale} from "$lib/paraglide/runtime";
+    import {locales, localizeHref, getLocale} from "$lib/paraglide/runtime";
     import {page} from "$app/state";
 
     const currentLocale = getLocale();
@@ -10,10 +10,10 @@
 <ul class="uppercase flex gap-x-4">
     {#each otherLocales as locale (locale)}
         <li>
-            <a href={localizeHref(page.url.pathname, {locale}).replace(/\/$/, '')}
+            <a href={localizeHref(page.url.pathname, {locale}).replace(/\/$/, '') || '/'}
                hreflang={locale}
+               data-sveltekit-reload
                class="no-underline py-2 inline-block"
-               onclick={() => setLocale(locale)}
             >
                 {m.language_name({}, {locale})}
             </a>
