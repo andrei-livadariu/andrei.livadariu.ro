5bc063b0fd8db32eef33e1fcf387c69eeed1c805
Andrei Livadariu <livadariu.andrei@gmail.com>
2025-05-23 18:31:05 +0900

    Optimization - let Vite process the background image so it adds a cache busting token

--- src/lib/components/blocks/Background.svelte
+++ src/lib/components/blocks/Background.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
     import type {Snippet} from "svelte";
+    import bg from '$lib/assets/background-optimized.svg';
 
     const { children }: {children: Snippet} = $props();
 </script>
 
-<div class="background">
+<div class="background" style="background-image: linear-gradient(44deg, rgba(1, 113, 176, .9) 34%, rgba(135, 255, 200, 0.85)), url({bg});">
     {@render children()}
 </div>
 
@@ -12,7 +13,6 @@
     .background {
         position: relative;
         background-repeat: repeat;
-        background-image: linear-gradient(44deg, rgba(1, 113, 176, .9) 34%, rgba(135, 255, 200, 0.85)), url(/images/background-optimized.svg);
         background-attachment: scroll, fixed;
         color: #fff;
     }
